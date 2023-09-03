8306221fa21e13d1a7906c87fa637a16d2cbbcbe
Daniel Alfaro <geberthalfaro85@gmail.com>
2023-09-03 16:17:13 -0600

    Creación de la interfaz del login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Signin from './Components/Signin/Signin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
+      <Signin/>
     </div>
   );
 }
