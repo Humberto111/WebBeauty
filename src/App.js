859b455e8ce6c180ef56a8ad19d704b2f3276e34
Humberto24 <humberto.vaco_24@hotmail.com>
2023-09-28 17:58:59 -0600

    Creación de la página principal y el responsive, crud de productos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import './App.css';
 import Signin from './Components/Signin/Signin';
 import Register from './Components/Register/Register';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Navigation from './Components/Navigation/Navigation';
+import ShowProducts from './Components/Productos/ShowProducts/ShowProducts';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/test" element={<div>Hello</div>} />
-          <Route path="/" element={<Signin />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="app">
+        <Sidebar />
+        <ShowProducts/>
     </div>
   );
 }
