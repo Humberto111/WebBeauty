7b3a3c2912efe317c26edb5434d466cd15036196
Geberth Daniel Alfaro Sequeira <geberth.alfaro@quarzo.com>
2023-11-18 21:17:20 -0600

    Se agrega funcionalidad para mostrar y realizar comentarios al 80%

--- src/layouts/comentarios/index.js
+++ src/layouts/comentarios/index.js
@@ -1,142 +1,151 @@
-import { useState } from "react";
-
-// @mui material components
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-
-// Material Dashboard 2 React components
+import React, { useEffect, useState } from "react";
+import { Grid, Card, Typography, Box, Alert, Button, TextField } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAlert from "components/MDAlert";
-import MDButton from "components/MDButton";
-import MDSnackbar from "components/MDSnackbar";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 
 function Notifications() {
-  const [successSB, setSuccessSB] = useState(false);
-  const [infoSB, setInfoSB] = useState(false);
-  const [warningSB, setWarningSB] = useState(false);
-  const [errorSB, setErrorSB] = useState(false);
+  const [comentario, setComentario] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const [responseText, setResponseText] = useState("");
+  const [responseTexts, setResponseTexts] = useState({});
 
-  const openSuccessSB = () => setSuccessSB(true);
-  const closeSuccessSB = () => setSuccessSB(false);
-  const openInfoSB = () => setInfoSB(true);
-  const closeInfoSB = () => setInfoSB(false);
-  const openWarningSB = () => setWarningSB(true);
-  const closeWarningSB = () => setWarningSB(false);
-  const openErrorSB = () => setErrorSB(true);
-  const closeErrorSB = () => setErrorSB(false);
+  const getComment = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/coments", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setComentario(data);
+        console.log(data);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const alertContent = (name) => (
-    <MDTypography variant="body2" color="white">
-      A simple {name} alert with{" "}
-      <MDTypography component="a" href="#" variant="body2" fontWeight="medium" color="white">
-        an example link
-      </MDTypography>
-      . Give it a click if you like.
-    </MDTypography>
-  );
+  const handleCommentSubmit = async () => {
+    try {
+      const user = JSON.parse(localStorage.getItem("users"));
+      const response = await fetch("http://localhost:3001/addComent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ texto: newComment, usuario_id: user.id, estado: "activo" }),
+      });
 
-  const renderSuccessSB = (
-    <MDSnackbar
-      color="success"
-      icon="check"
-      title="Material Dashboard"
-      content="Hello, world! This is a notification message"
-      dateTime="11 mins ago"
-      open={successSB}
-      onClose={closeSuccessSB}
-      close={closeSuccessSB}
-      bgWhite
-    />
-  );
+      if (response.ok) {
+        // Refresh comments after successful submission
+        getComment();
+        setNewComment("");
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const renderInfoSB = (
-    <MDSnackbar
-      icon="notifications"
-      title="Material Dashboard"
-      content="Hello, world! This is a notification message"
-      dateTime="11 mins ago"
-      open={infoSB}
-      onClose={closeInfoSB}
-      close={closeInfoSB}
-    />
-  );
+  const handleResponseSubmit = async (commentId) => {
+    try {
+      const user = JSON.parse(localStorage.getItem("users"));
+      const response = await fetch(`http://localhost:3001/responseComent`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          texto: responseTexts[commentId],
+          respuesta_a: commentId,
+          estado: "activo",
+          usuario_id: user.id,
+        }),
+      });
 
-  const renderWarningSB = (
-    <MDSnackbar
-      color="warning"
-      icon="star"
-      title="Material Dashboard"
-      content="Hello, world! This is a notification message"
-      dateTime="11 mins ago"
-      open={warningSB}
-      onClose={closeWarningSB}
-      close={closeWarningSB}
-      bgWhite
-    />
-  );
+      if (response.ok) {
+        // Refresh comments after successful submission
+        getComment();
+        setResponseText("");
+        setResponseTexts((prevState) => ({
+          ...prevState,
+          [commentId]: "",
+        }));
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const renderErrorSB = (
-    <MDSnackbar
-      color="error"
-      icon="warning"
-      title="Material Dashboard"
-      content="Hello, world! This is a notification message"
-      dateTime="11 mins ago"
-      open={errorSB}
-      onClose={closeErrorSB}
-      close={closeErrorSB}
-      bgWhite
-    />
-  );
+  useEffect(() => {
+    getComment();
+  }, []);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <MDBox mt={6} mb={3}>
-        <Grid container spacing={3} justifyContent="center">
-          <Grid item xs={12} lg={8}>
-            <Card>
-              <MDBox p={2}>
-                <MDTypography variant="h5">Comentarios</MDTypography>
-              </MDBox>
-              <MDBox pt={2} px={2}>
-                <MDAlert color="primary" dismissible>
-                  {alertContent("primary")}
-                </MDAlert>
-                <MDAlert color="secondary" dismissible>
-                  {alertContent("secondary")}
-                </MDAlert>
-                <MDAlert color="success" dismissible>
-                  {alertContent("success")}
-                </MDAlert>
-                <MDAlert color="error" dismissible>
-                  {alertContent("error")}
-                </MDAlert>
-                <MDAlert color="warning" dismissible>
-                  {alertContent("warning")}
-                </MDAlert>
-                <MDAlert color="info" dismissible>
-                  {alertContent("info")}
-                </MDAlert>
-                <MDAlert color="light" dismissible>
-                  {alertContent("light")}
-                </MDAlert>
-                <MDAlert color="dark" dismissible>
-                  {alertContent("dark")}
-                </MDAlert>
-                <MDAlert color="dark" dismissible>
-                  {alertContent("dark")}
-                </MDAlert>
-              </MDBox>
-            </Card>
-          </Grid>
-        </Grid>
+      {/* Formulario para Comentarios */}
+      <MDBox p={2}>
+        <TextField
+          label="Escribe tu comentario"
+          multiline
+          rows={4}
+          variant="outlined"
+          fullWidth
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+        />
+        <Box mt={2}>
+          <Button variant="contained" color="primary" onClick={handleCommentSubmit}>
+            Comentar
+          </Button>
+        </Box>
+      </MDBox>
+      {/* Visualización de Comentarios */}
+      <MDBox pt={2} px={2}>
+        {comentario.map((comment) => (
+          <Box key={comment.comentario_id} mb={2}>
+            <Alert color="primary" dismissible>
+              <Typography variant="body2">{comment.texto}</Typography>
+            </Alert>
+            {/* Formulario para Responder a Comentario */}
+            <Box mt={2} p={2}>
+              <TextField
+                label={"Responder al comentario " + comment.comentario_id}
+                multiline
+                rows={3}
+                variant="outlined"
+                fullWidth
+                value={responseTexts[comment.comentario_id] || ""}
+                onChange={(e) =>
+                  setResponseTexts((prevState) => ({
+                    ...prevState,
+                    [comment.comentario_id]: e.target.value,
+                  }))
+                }
+              />
+              <Box mt={2}>
+                <Button
+                  variant="contained"
+                  color="info"
+                  onClick={() => handleResponseSubmit(comment.comentario_id)}
+                >
+                  Responder
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        ))}
       </MDBox>
       <Footer />
     </DashboardLayout>
